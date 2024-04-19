f24bdc90d3d3fb4dd24428293d00d3ee4bc03012
Reiko Lee <sinreiko@hotmail.com>
2024-04-19 21:13:25 +0800

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import theme from '@chakra-ui/theme'; // Chakra UI theme
+import { extendTheme } from '@chakra-ui/react'; // Import the specific named export 'extendTheme' from '@chakra-ui/react'
 import React from 'react';
+
+const theme = extendTheme(); // Create a Chakra UI theme using the 'extendTheme' function
 
 function MyApp({ Component, pageProps }: { Component: React.ElementType, pageProps: any }) {
     return (
