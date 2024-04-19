1a7a72c274f5f2ff2d8953540d1fd2bb0dcc72c8
Reiko Lee <sinreiko@hotmail.com>
2024-04-19 20:41:09 +0800

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import theme from '@chakra-ui/theme'; // Chakra UI theme
 import React from 'react';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: { Component: React.ElementType, pageProps: any }) {
     return (
         <ChakraProvider theme={theme}>
             <Component {...pageProps} />
